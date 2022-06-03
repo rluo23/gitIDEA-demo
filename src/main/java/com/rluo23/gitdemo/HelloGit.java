742fab2622350db46b3d60bc7447705021a304b1
rluo <lrh198681@gmail.com>
2022-06-03 14:39:53 +0800

    first commit from IDEA

---
+++ src/main/java/com/rluo23/gitdemo/HelloGit.java
@@ -0,0 +1,11 @@
+package com.rluo23.gitdemo;
+
+/**
+ * @author rluo
+ * @date 2022/6/3 - 14:37
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
