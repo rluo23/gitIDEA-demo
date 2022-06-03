6f8edd31a5c2028c15acfe7cead8a06d9ebebe17
rluo <lrh198681@gmail.com>
2022-06-03 14:42:18 +0800

    second commit from IDEA

--- src/main/java/com/rluo23/gitdemo/HelloGit.java
+++ src/main/java/com/rluo23/gitdemo/HelloGit.java
@@ -7,5 +7,6 @@ package com.rluo23.gitdemo;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
